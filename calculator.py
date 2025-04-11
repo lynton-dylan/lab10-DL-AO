f9056aa9f3c69510cd020f7158e756fbf776e135
lynton-dylan <lyntondylan@gmail.com>
2025-04-11 16:40:39 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,42 @@
+#https://github.com/lynton-dylan/lab10-DL-AO.git
+#Partner 1: Dylan Lynton
+#Partner 2: Aaliyah Otto
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
 
+import math
 
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    try:
+        if a == 0:
+            raise ZeroDivisionError
+        else:
+            return b / a
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero!")
+
+def logarithm(a, b):
+    try:
+        if b <= 0 or a <= 0 or a == 1:
+            raise ValueError
+        else:
+            return math.log(b, a)
+    except ValueError:
+        print("Error: Invalid numbers for logarithm.")
+
+def exponent(a, b):
+    return a ** b
